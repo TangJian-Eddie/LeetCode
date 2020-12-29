afe8e612d87f9faaf237461d763a3374f1cbf9aa
TangJian-Eddie <905286484@qq.com>
2020-12-29 10:27:00 +0800

    feat: LeetCode 330

---
+++ 330.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number}
+ */
+var minPatches = function (nums, n) {
+  let patches = 0;
+  let x = 1;
+  const length = nums.length;
+  let index = 0;
+  while (x <= n) {
+    if (index < length && nums[index] <= x) {
+      x += nums[index];
+      index++;
+    } else {
+      x *= 2;
+      patches++;
+    }
+  }
+  return patches;
+};
