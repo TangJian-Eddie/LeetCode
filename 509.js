794997af2a2f28e2584d60b070d7a8d34cf74ccd
TangJian-Eddie <905286484@qq.com>
2021-01-04 10:36:03 +0800

    feat: LeetCode 509

---
+++ 509.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function (n) {
+  if (n == 0) {
+    return 0;
+  }
+  if (n == 1) {
+    return 1;
+  }
+  function Fibonacci2(n, ac1 = 0, ac2 = 1) {
+    if( n <= 1 ) {return ac2};
+    return Fibonacci2(n - 1, ac2, ac1 + ac2);
+  }
+  return Fibonacci2(n)
+};
+console.log(fib(4));
